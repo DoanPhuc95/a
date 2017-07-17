e5e3e3fe9747260eca5aa7bf37a1f693c708cb0a
DoanPhuc95 <phucdm1901@gmail.com>
2017-07-17 16:02:48 +0700

    step2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutes } from './app.routes';
-
+import { I18nModule } from './app.i18n';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
@@ -20,7 +20,8 @@ import { HeaderComponent } from './header/header.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutes
+    AppRoutes,
+    I18nModule
   ],
   providers: [],
   bootstrap: [AppComponent]
